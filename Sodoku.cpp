fa52d28beb0be0eb052354a7cb5c0bc1b60aa20f
hak8or <hak8or@gmail.com>
2014-03-15 12:07:36 -0400

    Implemented displaying of the Sudoku puzzle.

--- Sodoku.cpp
+++ Sodoku.cpp
@@ -1,5 +1,10 @@
 #include "Sodoku.h"
 #include "Square_Matrix.h"
+#include <iostream>
+
+// Gotta use a namespace or else all my couts will have to specify the namespace
+// each time.
+using namespace std;
 
 // Constructor for the sodoku object.
 sodoku::sodoku(void){
@@ -15,7 +20,29 @@ int sodoku::Get_Size(void){
 	return this->matrix.Get_Size();
 }
 
-// Throws out the current sodoku puzzle's contents.
+// Throws out the current sodoku puzzle contents by going row through row, and
+// adding spacing between each cells contents for making it easier to read.
 void sodoku::display(void){
+	// For saying that these are the contents.
+	cout << "Contents of puzzle: " << endl;
 
+	// Will hold the current row.
+	vector<int> row_contents;
+
+	// Go through each row.
+	for (int row = 0; row < this->matrix.Get_Size(); ++row)
+	{
+		// Copies the row into a vector.
+		row_contents = this->matrix.Get_Row(row);
+
+		// Makes some nice formatting to the start of the row.
+		cout << "| ";
+
+		// Displays the contents of each cell of the row with right hand padding.
+		for (int cell_count = 0; cell_count < this->matrix.Get_Size(); ++cell_count)
+			cout << row_contents[cell_count] << " ";
+
+		// Shows that the row is done.
+		cout << "|" << endl;
+	}
 }
